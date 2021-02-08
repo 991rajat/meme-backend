9c5369dfa03cad0b26b154fcf6341ac6f6cb90f4
Rajat Kumar <991rajat@gmail.com>
2021-02-08 16:52:19 +0530

    initial commit (backend setup)

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+//  Env File
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION_URL = process.env.MONGO_URI;
+
+// DB Connection
+mongoose
+  .connect(CONNECTION_URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    userFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log(
+      `Mongoose Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  })
+  .catch((err) => {
+    console.log(`Error occured: ${err}`.red.bold);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}.`.yellow.bold);
+});
